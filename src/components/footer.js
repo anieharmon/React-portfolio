86502cfc5b098f3d8af649723f7d99a4f7a0fb1a
anieharmon <anieharmon@icloud.com>
2023-08-08 17:30:34 -0600

    projects

---
+++ src/components/footer.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>
+          <strong>My Full Stack Development Portfolio</strong> - a React site by{" "}
+          <a href="https://github.com/anieharmon target="_blank" rel="noreferrer">
+            Anie Harmon
+          </a>
+          .
+          <hr />
+          <a
+            href="www.linkedin.com/in/anie-harmon-1554b6246"
+            target="_blank" rel="noreferrer"
+          >
+            LinkedIn
+         
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
